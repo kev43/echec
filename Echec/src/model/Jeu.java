5344a717c59ced53718a8178a9103598c8762106
kev43 <kev43200@gmail.com>
2017-05-31 10:31:19 +0200

    Modification de la classe Jeu (classes isMoveOK , move)

--- Echec/src/model/Jeu.java
+++ Echec/src/model/Jeu.java
@@ -5,7 +5,6 @@
  */
 package model;
 
-import static java.util.Collections.list;
 import java.util.LinkedList;
 import java.util.List;
 import static tools.ChessPiecesFactory.newPieces;
@@ -16,95 +15,132 @@ import static tools.ChessPiecesFactory.newPieces;
  */
 public class Jeu implements Game {
 
-    private List<Pieces> listePieces ;
+    private List<Pieces> listePieces;
     Couleur couleur;
-            
-            
+    boolean castlingPossible = false;
+    boolean possibleCapture = false;
+
     public Jeu(Couleur couleur) {
-        this.couleur = couleur ;
+        this.couleur = couleur;
         this.listePieces = newPieces(couleur);
     }
-    
+
     @Override
     public boolean isPieceHere(int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Pieces p : listePieces) {
+            if (p.getX() == x && p.getY() == y) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         for (Pieces p : listePieces) {
+            if (p.getX() == xInit && p.getY() == yInit) {
+                return p.isMoveOk(xFinal, yFinal, isCatchOk, isCastlingPossible);
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        //isMoveOk()xInit , yInit , xFinal , yFinal , capture(xFinal , yFinal) , isCastlingPossible);
+        //rajouter la condition isMoveOK
+        for (Pieces p : listePieces) {
+            if (p.getX() == xInit && p.getY() == yInit) {
+                p.move(xFinal, yFinal);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
     public boolean capture(int xCatch, int yCatch) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /*for (Pieces p : listePieces) {
+            if (p.getX() == xCatch && p.getY() == yCatch) {
+                return true;
+            }
+        }
+
+        return false;*/
+        return false;
     }
-    
-    
+
     public void setPossibleCapture() {
-        
+
     }
-    
-    public Couleur getPieceColor(int x , int y) {
+
+    public Couleur getPieceColor(int x, int y) {
+        for (Pieces p : listePieces) {
+            if (p.getX() == x && p.getY() == y) {
+                return p.getCouleur();
+            }
+        }
         return null;
-        
     }
-    
-    public String getPieceName( int x , int y) {
+
+    public String getPieceName(int x, int y) {
+        for (Pieces p : listePieces) {
+            if (p.getX() == x && p.getY() == y) {
+                return p.getClass().getSimpleName();
+            }
+        }
         return null;
-        
+
     }
-    
+
     public Couleur getCouleur() {
-        return null;
-        
+        return this.couleur;
+
     }
-    
+
     public void setCastling() {
         
     }
-    
+
     public List<PieceIHMs> getPiecesIHM() {
         PieceIHMs newPieceIHM = null;
         List<PieceIHMs> list = new LinkedList<>();
-        for (Pieces piece : listePieces){
+        for (Pieces piece : listePieces) {
             //si la piece est toujours en jeu
-            if (piece.getX()!= -1) {
+            if (piece.getX() != -1) {
                 newPieceIHM = new PieceIHM(piece);
                 list.add(newPieceIHM);
             }
         }
         return list;
     }
-    
-    public static void main(java.lang.String[] args){
+
+    public static void main(java.lang.String[] args) {
         Jeu j = new Jeu(Couleur.NOIR);
         j.toString();
     }
-    
+
     @Override
     public String toString() {
-        for(Pieces p : listePieces) {
-            p.toString();
+        String S = "";
+        for (Pieces p : listePieces) {
+            S += p.toString();
         }
-        return null;
-        
+        return S;
+
     }
-    
-    private Pieces findPiece(int x , int y) {
-        for (Pieces p : listePieces){
-            if(p.getX()==x && p.getY()==y){
+
+    private Pieces findPiece(int x, int y) {
+        for (Pieces p : listePieces) {
+            if (p.getX() == x && p.getY() == y) {
                 return p;
             }
         }
-        
+
         return null;
-        
+
     }
-    
+
 }
